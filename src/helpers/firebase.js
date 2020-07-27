0144d3c8b85309f376db7115f256a6bbb79ea7f4
Daniel Lamarr <danielchidiebele@gmail.com>
2020-07-28 00:32:05 +0100

    deploy GOOGLE credentials

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../config/serviceAccountKey.json';
+import serviceAccount from '/app/google-credentials.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
